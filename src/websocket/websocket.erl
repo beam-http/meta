c6886e4c166f67bcc2eedca69891338c5ee7fb1a
vans163 <vans_163@yahoo.com>
2016-09-15 21:39:34 -0400

    small tuneup

--- src/websocket/websocket.erl
+++ src/websocket/websocket.erl
@@ -1,14 +1,10 @@
 -module(websocket).
 -compile(export_all).
 
-useless_hash(WSKey) when is_list(WSKey) ->
-    UselessHash = crypto:hash(sha, WSKey ++ "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"),
-    base64:encode(UselessHash);
 useless_hash(WSKey) ->
-    UselessHash = crypto:hash(sha, 
-        <<WSKey/binary, "258EAFA5-E914-47DA-95CA-C5AB0DC85B11">>),
-    base64:encode(UselessHash).
-
+    base64:encode(crypto:hash(sha, 
+        [WSKey, <<"258EAFA5-E914-47DA-95CA-C5AB0DC85B11">>])
+    ).
 
 
 
@@ -18,19 +14,15 @@ xor_payload(Payload, Mask) -> xor_payload(Payload, Mask, <<>>).
 xor_payload(<<>>, Mask, Acc) -> Acc;
 xor_payload(<<Chunk:32, Rest/binary>>, M= <<Mask:32>>, Acc) ->
     XorChunk = Chunk bxor Mask,
-    xor_payload(Rest, M, <<Acc/binary, XorChunk:32>>)
-    ;
+    xor_payload(Rest, M, <<Acc/binary, XorChunk:32>>);
 xor_payload(<<Chunk:24, Rest/binary>>, M= <<Mask:24, _/binary>>, Acc) ->
     XorChunk = Chunk bxor Mask,
-    xor_payload(Rest, M, <<Acc/binary, XorChunk:24>>)
-    ;
+    xor_payload(Rest, M, <<Acc/binary, XorChunk:24>>);
 xor_payload(<<Chunk:16, Rest/binary>>, M= <<Mask:16, _/binary>>, Acc) ->
     XorChunk = Chunk bxor Mask,
-    xor_payload(Rest, M, <<Acc/binary, XorChunk:16>>)
-    ;
+    xor_payload(Rest, M, <<Acc/binary, XorChunk:16>>);
 xor_payload(<<Chunk:8, Rest/binary>>, M= <<Mask:8, _/binary>>, Acc) ->
     XorChunk = Chunk bxor Mask,
-    xor_payload(Rest, M, <<Acc/binary, XorChunk:8>>)
-    .
+    xor_payload(Rest, M, <<Acc/binary, XorChunk:8>>).
 
 
